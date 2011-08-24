1d2a667c1c06606777f19a1c7571df97cb060a22
Santhosh Kumar <santho89@gmail.com>
2011-08-24 08:44:39 +0530

    Added a method to get the dump data and process it based on the upper and lower bound to parse the dump data separately

--- ConnectOneManMode/Form1.vb
+++ ConnectOneManMode/Form1.vb
@@ -21,4 +21,46 @@ Public Class Form1
             Application.Restart()
         End If
     End Sub
+
+    Sub sndCommand(ByVal cmdToExec As String, ByVal msg As String)
+        Dim tc As New TcpClient
+        Try
+            tc.Connect(devIp, prt)
+            Dim outStream As Byte() = System.Text.Encoding.ASCII.GetBytes(cmdToExec)
+            tc.GetStream().Write(outStream, 0, outStream.Length)
+            System.Threading.Thread.Sleep(2000)
+            execTransfer()
+            chngLog.Text = chngLog.Text + vbNewLine + msg
+            MessageBox.Show(msg)
+        Catch ex As Exception
+            MessageBox.Show("Cannot access management mode.Please try again")
+        End Try
+        tc.Close()
+    End Sub
+
+    Function GetDumpData()
+        Dim tc As New TcpClient
+        tc.Connect(devIp, prt)
+        Dim strmArr As String()
+        Dim dumpCmd As String = "* dump 0;"
+        Dim inStream(tc.ReceiveBufferSize) As Byte
+        Dim outStream As Byte() = System.Text.Encoding.ASCII.GetBytes(dumpCmd)
+        tc.GetStream().Write(outStream, 0, outStream.Length)
+        System.Threading.Thread.Sleep(3000)
+        tc.GetStream().Read(inStream, 0, CInt(tc.ReceiveBufferSize))
+        Dim returndata As String = _
+        System.Text.Encoding.ASCII.GetString(inStream)
+        tc.Close()
+        Dim rawData As String = returndata
+        Dim indx As Integer = rawData.IndexOf("connect-one")
+        indx = indx - 24
+        Dim indy As Integer = rawData.IndexOf("sdCardLockEnable")
+        indy = indy + 19
+        Dim str As String = rawData.Substring(indx, indy - indx)
+        'MessageBox.Show(indx)
+        'MessageBox.Show(str)
+        strmArr = str.Split("|")
+        Return strmArr
+    End Function
+
 End Class
