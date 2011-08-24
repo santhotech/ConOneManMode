acc02e031e9c817526cf08e3ed83d3d88a1abd00
Santhosh Kumar <santho89@gmail.com>
2011-08-24 08:42:59 +0530

    Added amethod to send data over the tcp port

--- ConnectOneManMode/Form1.vb
+++ ConnectOneManMode/Form1.vb
@@ -5,7 +5,20 @@ Public Class Form1
     Dim devIp As String
     Dim prt As Integer
     Dim met As New Methods
-    Private Sub Button55_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button55.Click
-        met.clearAll(Me)
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim butText As String = Button1.Text
+        devIp = cnctIpAddrTxt.Text
+        prt = prtTxt.Text
+        If butText = "Connect" Then
+            Try
+                execTransfer()
+                Button1.Text = "Disconnect"
+                MainPanel.Visible = True
+            Catch ex As Exception
+                MsgBox("Device is not reachable. Check whether the IP is correct and is in the same network as client.")
+            End Try
+        ElseIf butText = "Disconnect" Then
+            Application.Restart()
+        End If
     End Sub
 End Class
